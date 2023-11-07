799c38eeb05fb528cad300e424aba9a9912e4d76
Mahimna <upadhyaymahi28@gmail.com>
2023-11-07 17:35:06 +0530

    Changes made

--- script.js
+++ script.js
@@ -3,6 +3,19 @@ import * as THREE from './node_modules/three';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
+const textureLoader = new THREE.TextureLoader();
+const spaceBackgroundTexture = textureLoader.load('./spacebg.jpg');
+
+const spaceBackgroundSphere = new THREE.Mesh
+(
+	new THREE.SphereGeometry(60,32,30), // Adjust the radius and segments as needed
+	new THREE.MeshBasicMaterial({ map: spaceBackgroundTexture, side: THREE.BackSide })
+);
+  
+scene.add(spaceBackgroundSphere);
+
+camera.position.z= 60;
+
 const renderer = new THREE.WebGLRenderer(
 	{
 		alpha: true
@@ -20,16 +33,16 @@ const sphere = new THREE.Mesh(
 	new THREE.MeshBasicMaterial( {map:texture} )
 
 );
-
+  
 scene.add( sphere );
 
-camera.position.z = 30;
+camera.position.z = 60;
 
 function animate() {
 	
 	requestAnimationFrame( animate );
 
-	sphere.rotation.y += 0.01;
+	sphere.rotation.y += 0.002;
 
 	renderer.render( scene, camera );
 }
